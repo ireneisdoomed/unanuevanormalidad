59c9eb10a505ec14796c79926d6268255821ca0d
ireneisdoomed <irene.lopezs@protonmail.com>
2020-05-11 17:57:21 +0200

    Error handling OK

---
+++ Una Nueva Normalidad.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from PIL import Image
+from src.query import query
+
+header = Image.open("images/header.png")
+
+st.image(header, width=800, format="PNG",  use_column_width=True)
+
+st.header("¿Cuál es tu código postal?")
+code = st.text_input("Inserta aquí tu código postal.")
+
+if len(code) == 5:
+    try:
+        fase, territory = query(code)
+        st.write(f"El código {code} corresponde a {territory}.")
+
+        if str(fase) == "1":
+            info = open('data/fase1.md', 'r')
+            st.markdown(info.read())
+
+        elif str(fase) == "0":
+            info = open("data/fase0.md", "r")
+            st.markdown(info.read())
+    except Exception:
+        st.write("🚫 Este código postal no es válido. Por favor, vuelve a intentarlo.")
+    
+elif len(code) != 0 and len(code) != 5:
+    st.write("El código debe ser de 5 dígitos. Por favor, vuelve a intentarlo.")
+
+
+
+st.markdown("""
+        
+
+        Written with 🧡 by [Irene López](https://github.com/ireneisdoomed).
+
+        """)
--- src/query.py
+++ src/query.py
@@ -3,21 +3,28 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost/unanuevanormalidad")
 db = client.get_database()
 
+
 def query(zipcode):
     q = list((db["data"].find({"$and": [{"CP": zipcode[:2]}, {"Excepcion": False}]})))
 
+    # Provinces without exceptions
     if len(q) > 0:
-        result = q[0]["Fase"]
+        fase = q[0]["Fase"]
         territory = q[0]["Provincia"]
-        return result, territory
+        return fase, territory
+
+    # Provinces with exceptions
     else:
         q2 = list((db["data"].find({"$and": [{"CP": zipcode[:2]}, {"Excepcion": {"$ne": False}}]},
-                            {"Excepcion":1, "_id":0})))
-        if len(q2) == 0:
-            result = "0"
-            print(result)
-            return result
+                                {"Excepcion":1, "_id":0})))
+
+        if q2[0]["Excepcion"].get(zipcode) != None:
+                fase = q2[0]["Excepcion"][zipcode]["Fase"]
+                territory = q2[0]["Excepcion"][zipcode]["Territorio"]
+                return fase, territory
         else:
-            result = q2[0]["Excepcion"][zipcode]["Fase"]
-            territory = q2[0]["Excepcion"][zipcode]["Territorio"]
-            return result, territory
+            q3 = list((db["data"].find({"CP": zipcode[:2]})))
+            fase = "0"
+            territory = q3[0]["Provincia"]
+            return fase, territory
+    
